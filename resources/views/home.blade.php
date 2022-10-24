d8a4389749fedb3eacf41c754a2c743df9eab62b
renzcabato1 <cabato.renz.renz@gmail.com>
2022-10-24 10:29:09 +0800

    Done

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -17,11 +17,11 @@
                                             <h4 class="card-title">Web Applications</h4>
                                             <hr>
                                         </div>
-                                        <div class="col-xl-6 mb-3">
+                                        <div class="col-xl-8 mb-3">
                                             <span class="badge badge-pill badge-outline-success text-success mb-2">Link 1</span> - Link Inside PMI Tower <br>
                                             <span class="badge badge-pill badge-outline-info text-info">Link 2</span> - Link Outside PMI Tower
                                         </div>
-                                        <div class="col-xl-6 mb-3">
+                                        <div class="col-xl-4 mb-3">
                                            
                                             <div class="input-group">
                                                 <input type="text" class="form-control form-control-sm" id="Search"
